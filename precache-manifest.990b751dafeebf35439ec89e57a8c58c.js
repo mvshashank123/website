e686302d8b6285b1b96df52e8d02eb6075983c27
Shashank <mv.shashank123@gmail.com>
2021-02-25 11:52:40 +0530

    Updates

---
+++ precache-manifest.990b751dafeebf35439ec89e57a8c58c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e60ebe6b1a289c0647960cc513026a",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "ca98ff6f648d0679f610",
+    "url": "/website/static/css/main.9d794227.chunk.css"
+  },
+  {
+    "revision": "4dddacf9dece2bc4a8f9",
+    "url": "/website/static/js/2.d50f264c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/website/static/js/2.d50f264c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca98ff6f648d0679f610",
+    "url": "/website/static/js/main.412e05d4.chunk.js"
+  },
+  {
+    "revision": "f7ba02939af9c7e088f6",
+    "url": "/website/static/js/runtime-main.d57c616e.js"
+  },
+  {
+    "revision": "96a4a08dada1c260d3d9f885b3f690d3",
+    "url": "/website/static/media/Profile.96a4a08d.jpg"
+  },
+  {
+    "revision": "51c07cbde6020c34cc6b4bbf7133b9da",
+    "url": "/website/static/media/amazon.51c07cbd.png"
+  },
+  {
+    "revision": "749e5c805a8fe132d4e988d5eb3a62af",
+    "url": "/website/static/media/covidtr.749e5c80.png"
+  },
+  {
+    "revision": "e661fdc0203a187bcc2f725560fe45e6",
+    "url": "/website/static/media/messaging.e661fdc0.png"
+  },
+  {
+    "revision": "1d1d0a212652abd6cd28f1f27bf7c368",
+    "url": "/website/static/media/netflix.1d1d0a21.png"
+  },
+  {
+    "revision": "9574bf106657b18b66ad88390e30dc11",
+    "url": "/website/static/media/resume.9574bf10.pdf"
+  },
+  {
+    "revision": "c4da5bf8850f2508ee70d37ca1a8930c",
+    "url": "/website/static/media/spotify.c4da5bf8.png"
+  }
+]);
