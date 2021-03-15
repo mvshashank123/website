a1360ef63a01c30a7b3ef83e19723b2b9e7459ae
Shashank <mv.shashank123@gmail.com>
2021-03-15 08:55:28 +0530

    changes

--- home-master/src/components/blog/Mui.js
+++ home-master/src/components/blog/Mui.js
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
-import Avatar from "@material-ui/core/Avatar";
-import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import { Jumbotron } from "react-bootstrap"
 import Tracker from '../home/images/covidtr.png'
 
--- home-master/src/components/blog/Widget.js
+++ home-master/src/components/blog/Widget.js
@@ -1,25 +1,26 @@
 import React, { useEffect } from "react";
 import { Jumbotron } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-import './Widget.css'
+import "./Widget.css";
 
 const Widget = () => {
-
   useEffect(() => {
     window.mediumWidget();
   }, []);
 
-  return <Jumbotron
-  fluid
-  id="blogs"
-  className="jumbotron jumbotron-fluid m-0"
-  style={{ backgroundColor: "white" }}
->
-       <Container className="">
-       <h2 className="display-4 pb-5 text-center">Latest Articles</h2>
-         <div id='medium-widget'></div>
-       </Container>
-  </Jumbotron>;
+  return (
+    <Jumbotron
+      fluid
+      id="blogs"
+      className="jumbotron jumbotron-fluid m-0"
+      style={{ backgroundColor: "white" }}
+    >
+      <Container className="">
+        <h2 className="display-4 pb-5 text-center">Latest Articles</h2>
+        <div id="medium-widget"></div>
+      </Container>
+    </Jumbotron>
+  );
 };
 
 export default Widget;
