1838f22283487a28fe8f46166fe266113e35b6ab
Shashank M V <mv.shashank123@gmail.com>
2023-06-11 22:51:07 +0530

    Updates

--- precache-manifest.5751c0b4140800ec837fe81a24bb3cc9.js
+++ precache-manifest.5751c0b4140800ec837fe81a24bb3cc9.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0ce9e6d1204770a7d47247e1327cff33",
+    "revision": "73149d0e7384372c354d19f35f774c42",
     "url": "/website/index.html"
   },
   {
-    "revision": "d071429a7f650552131e",
-    "url": "/website/static/css/main.26251919.chunk.css"
+    "revision": "cc269216c2124d5c8ad6",
+    "url": "/website/static/css/main.0d770b32.chunk.css"
   },
   {
-    "revision": "07a469ea094e944642a5",
-    "url": "/website/static/js/2.b6bddd1a.chunk.js"
+    "revision": "cdb467584eb8c7440cf2",
+    "url": "/website/static/js/2.0140c9e5.chunk.js"
   },
   {
-    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
-    "url": "/website/static/js/2.b6bddd1a.chunk.js.LICENSE.txt"
+    "revision": "d5b473ff893d6b5fac7d0b5081a5072a",
+    "url": "/website/static/js/2.0140c9e5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d071429a7f650552131e",
-    "url": "/website/static/js/main.c8bd7f64.chunk.js"
+    "revision": "cc269216c2124d5c8ad6",
+    "url": "/website/static/js/main.afa568a8.chunk.js"
   },
   {
-    "revision": "f7ba02939af9c7e088f6",
-    "url": "/website/static/js/runtime-main.d57c616e.js"
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/website/static/js/main.afa568a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9148a61ea76eb4985b95",
+    "url": "/website/static/js/runtime-main.0d074f3d.js"
   },
   {
     "revision": "96a4a08dada1c260d3d9f885b3f690d3",
@@ -44,8 +48,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/website/static/media/netflix.1d1d0a21.png"
   },
   {
-    "revision": "a8193a0fe622310606dd3737fa99f61e",
-    "url": "/website/static/media/resume.a8193a0f.pdf"
+    "revision": "50835d0c9a56eaa6d92a80d737bd1c5b",
+    "url": "/website/static/media/resume.50835d0c.pdf"
   },
   {
     "revision": "c4da5bf8850f2508ee70d37ca1a8930c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.df24ded792c87495906742456199f515.js"
+  "/website/precache-manifest.5751c0b4140800ec837fe81a24bb3cc9.js"
 );
 
 self.addEventListener('message', (event) => {
