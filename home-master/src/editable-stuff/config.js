f965618f1e5f64a77591699151c325a98af073d1
Shashank M V <mv.shashank123@gmail.com>
2024-02-03 15:09:39 +0530

    Update config

--- home-master/src/editable-stuff/config.js
+++ home-master/src/editable-stuff/config.js
@@ -58,7 +58,7 @@ const about = {
   imageLink: require("../editable-stuff/Profile.jpg"),
   imageSize: 375,
   message:
-  "Hello! My name is Shashank, I'm currently working as a Software Engineer at Verizon. I work on building Web applications using React. I have a keen interest in Machine Learning and Artificial Intelligence. I also have a zeal for learning new technologies. The top portion of my attached resume highlights my career profile and the responsibilities I have had in my previous role.",
+  "Hello! My name is Shashank, I'm currently working as a Senior Engineer at Target. I work on building Web applications using React. I have a keen interest in Machine Learning and Artificial Intelligence. I also have a zeal for learning new technologies. The top portion of my attached resume highlights my career profile and the responsibilities I have had in my previous role.",
   resume: require("../editable-stuff/resume.pdf"),
 };
 
